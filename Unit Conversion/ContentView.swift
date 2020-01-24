79c56c09da4ba829ae946a9cb4c67b684590db3a
Lester Batres <swat1@mnc.gt>
2020-01-23 20:31:00 -0600

    Initial Commit

---
+++ Unit Conversion/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Unit Conversion
+//
+//  Created by Lester Batres on 1/23/20.
+//  Copyright © 2020 lester. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
