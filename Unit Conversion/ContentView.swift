22ef60bf98fd2fc920de7e0d0fe8a2bad6924eef
Lester Batres <lester96@gmail.com>
2020-01-23 21:36:48 -0600

    Unit Conversion
    -Convert from input unit to destination unit

--- Unit Conversion/ContentView.swift
+++ Unit Conversion/ContentView.swift
@@ -9,8 +9,61 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var inputUnit = 0
+    @State private var outputUnit = 1
+    @State private var length = "0"
+    
+    let unitsDescription = ["Meters", "Kilometers", "Feet", "Yards", "Miles"]
+    let units = [UnitLength.meters, UnitLength.kilometers, UnitLength.feet, UnitLength.yards, UnitLength.miles]
+    
+    var sourceLength: Measurement<UnitLength> {
+        let length = Double(self.length) ?? 0
+        let sourceUnit = units[inputUnit]
+        let sourceLenght = Measurement(value: length, unit: sourceUnit)
+        
+        return sourceLenght
+    }
+    
+    var destinationLength: Measurement<UnitLength> {
+        let destinationUnit = units[outputUnit]
+        let destinationLength = sourceLength.converted(to: destinationUnit)
+        
+        return destinationLength
+    }
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            Form {
+                Section(header: Text("Source Unit")) {
+                    Picker("Source unit", selection: $inputUnit) {
+                        ForEach(0 ..< units.count) {
+                            Text("\(self.unitsDescription[$0])")
+                        }
+                    }
+                .pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section(header: Text("Output Unit")) {
+                    Picker("Output unit", selection: $outputUnit) {
+                        ForEach(0 ..< units.count) {
+                            Text("\(self.unitsDescription[$0])")
+                        }
+                    }
+                .pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section(header: Text("Enter lenght")) {
+                    TextField("0", text: $length)
+                        .keyboardType(.decimalPad)
+                }
+                
+                Section(header: Text("Converted length")) {
+                    Text("\(destinationLength.description)")
+                }
+            }
+        .navigationBarTitle("Unit Conversion")
+        }
     }
 }
 
